08c3cdae8433e6f2cd074616e8c748304e129798
MrDragon88 <72425361+MrDragon88@users.noreply.github.com>
2020-11-11 16:45:00 -0600

    Creacion de Clase Empleado

---
+++ ProyectoGrupo/src/main/java/Package/cls_Empleado.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package;
+
+/**
+ *
+ * @author Oscar Andrade
+ */
+public class cls_Empleado {
+    
+    //Atributos
+    
+    private String _nombre;
+    private int _tiempolaboral;
+    private char _genero;
+    
+    
+}
