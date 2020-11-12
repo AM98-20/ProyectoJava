068794865ff6aa24c025a77f429451fb14b45691
WalterIran <amadorwalter3418@gmail.com>
2020-11-11 22:01:29 -0600

    solo falta el metodo de caluclo de prestaciones en la clase empleado administrativo. Se creó el formulario de empleado administrativo

--- ProyectoGrupo/src/main/java/Package/cls_EmpleadoAdmi.java
+++ ProyectoGrupo/src/main/java/Package/cls_EmpleadoAdmi.java
@@ -9,6 +9,31 @@ package Package;
  *
  * @author Walter Amador
  */
-public class cls_EmpleadoAdmi {
+public class cls_EmpleadoAdmi extends cls_Empleado{
     
+    private static double aguinaldo = 0;
+    private static double decimocuarto = 0;
+    private static double preAviso = 0;
+    private static double cesantia = 0;
+    private static double totalPrestaciones;
+    
+    public void calcularAguinaldo(){
+        cls_EmpleadoAdmi.aguinaldo = getSueldo() * 0.5;
+    }
+    
+    public void calcularDecimocuarto(){
+        cls_EmpleadoAdmi.decimocuarto = getSueldo() * 0.5;
+    }
+    
+    public void calcularPreAviso(boolean avisado){
+        if(avisado){
+            cls_EmpleadoAdmi.preAviso = 0;
+        }else{
+            cls_EmpleadoAdmi.preAviso = getSueldo() * 2;
+        }
+    }
+    
+    public void calcularCesantia(){
+        cls_EmpleadoAdmi.cesantia = getSueldo() * getTiempolaboral();
+    }
 }
